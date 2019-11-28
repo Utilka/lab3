fb745e447e9c65191dc94f9410670c06d21c7f0d
Utilka <andrii.v.bolshakov@gmail.com>
2019-11-28 23:27:02 +0200

    js first try

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var item1 = document.querySelector("#item1");
+var item2 = document.querySelector("#item2");
+var item3 = document.querySelector("#item3");
+var item4 = document.querySelector("#item4");
+var item5 = document.querySelector("#item5");
+var item6 = document.querySelector("#item6");
+var item7 = document.querySelector("#item7");
+var curent_item = item1;
+
+
+var item_content1 = document.querySelector("#item_content1");
+var item_content2 = document.querySelector("#item_content2");
+var item_content3 = document.querySelector("#item_content3");
+var item_content4 = document.querySelector("#item_content4");
+var item_content5 = document.querySelector("#item_content5");
+var item_content6 = document.querySelector("#item_content6");
+var item_content7 = document.querySelector("#item_content7");
+var curent_item_content = item_content1;
+
+
+function change_page(to_what_item_change, to_what_item_content_change) {
+    curent_item.classList.remove('active');
+    to_what_item_change.classList.add('active');
+    curent_item = to_what_item_change;
+    // curent_item_content.classList.add('hiden');
+    // to_what_item_content_change.classList.remove('hiden');
+    curent_item_content = to_what_item_content_change;
+}
+
+item1.addEventListener("click", function() { change_page(item1, item_content1) });
+item2.addEventListener("click", function() { change_page(item2, item_content2) });
+item3.addEventListener("click", function() { change_page(item3, item_content3) });
+item4.addEventListener("click", function() { change_page(item4, item_content4) });
+item5.addEventListener("click", function() { change_page(item5, item_content5) });
+item6.addEventListener("click", function() { change_page(item6, item_content6) });
+item7.addEventListener("click", function() { change_page(item7, item_content7) });
